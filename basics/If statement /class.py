f87add55c46db4837d53e2fea5b9af9f67eecb97
Boldizsar Banfia <boyszy@Boldizsars-MBP.lan>
2021-10-05 15:18:14 +0100

    class if statement

---
+++ basics/If statement /class.py
@@ -0,0 +1,9 @@
+#ask the user for hte age
+print("Please neter your age")
+# read the user's response
+age = int(input())
+# chehck if the user is an adult 
+if age >=18: # the column means do the following
+     print("You are an adult")
+     print("Because you are 18 years old or greater")
+print("End of the program")
