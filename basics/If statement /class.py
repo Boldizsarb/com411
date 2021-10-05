b3a29e4daceaca9ec399251fcbb239ccce9a25c3
Boldizsar Banfia <boyszy@Boldizsars-MBP.lan>
2021-10-05 15:49:27 +0100

    if statement

--- basics/If statement /class.py
+++ basics/If statement /class.py
@@ -6,7 +6,9 @@ age = int(input())
 if age >=18: # the column means do the following
      print("You are an adult")
      print("Because you are 18 years old or greater")
+elif age >= 13:
+    print("You are a teneger")
 else: # again the column means do the following 
-    print("You are not an adult")
+    print("You are not a child")
     print("Because you are below 18")
 print("End of the program")
---
+++ basics/If statement /more if statement.py
@@ -0,0 +1,14 @@
+# ask the user if it is suny
+print("Is it sunny")
+is_sunny = input().lower()
+
+print("Is ther a breeze?")
+is_breeze = input().lower()
+
+if is_sunny == "yes" and is_breeze == "yes": #why two equal == sign
+    if is_breeze == "yes" 
+        print("Clothes are dry")
+    else:
+        print("clothes are drying slowly")
+else:
+    print("clothes are not dry")
