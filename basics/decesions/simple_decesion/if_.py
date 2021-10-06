fb77505565a267e30bc5ab1643c095421224e3e0
Boldizsar Banfia <boyszy@Boldizsars-MBP.lan>
2021-10-06 20:44:08 +0100

    klasjdff

---
+++ basics/decesions/simple_decesion/if_.py
@@ -0,0 +1,8 @@
+#asking what type of book do you like
+print("What type of book is this?")
+answer = input()
+if answer.lower() == "adventure":
+     print("I like adventure books!")
+else: exit()
+print('Finished reading book.')
+exit()
