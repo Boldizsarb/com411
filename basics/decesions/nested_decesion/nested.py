32c1ddca6adaeaa9c3d0843cfa20a317db1d5fcc
Boldizsar Banfia <boyszy@Boldizsars-MBP.lan>
2021-10-07 16:21:11 +0100

    nestception

--- basics/decesions/nested_decesion/nested.py
+++ basics/decesions/nested_decesion/nested.py
@@ -13,4 +13,19 @@ if cover_type == "soft":
 elif cover_type == "hard":
         print("Books with hard covers can be more expensive!")
 
+# another way with the same outcome:
 
+print("What type of cover does this book have?")
+cover_type = input()
+
+# Display appropriate message
+if cover_type == "soft":
+    print("Is the book perfect-bound?")
+    perfect_bound = input()
+
+    if perfect_bound == "yes":
+        print("Soft cover, perfect bound books are very popular!")
+    else:
+        print("Soft covers with coils or stitches are great for short books") 
+else:
+    print("Books with hard covers can be more expensive!")
