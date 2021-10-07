d0653cf793b3af9af12e9491ce4b079fa9b5a127
Banfia Boldizsar <5boldb36@solent.ac.uk>
2021-10-07 12:49:56 +0100

    skfjl

--- basics/decesions/simple_decesion/comparison_operators.py
+++ basics/decesions/simple_decesion/comparison_operators.py
@@ -9,7 +9,7 @@
 #if first_number < second_number:
    # print("The first number is the smallest")
 
-first_number = input ("Please enter the first number")
+first_number = input ("Please enter the first number") #the input do not have to be in another line
 second_number = input ("Please enter the second number")
 if first_number > second_number:
     print("The second number is the smallest.")
---
+++ basics/decesions/simple_decesion/modulo_operator.py
@@ -0,0 +1,8 @@
+print("Please enter a whole number.")
+if (number % 2) == 0:
+    print("number {number} is an odd number")
+
+    #what f string is formatting string?
+    # age = int(input(())
+
+    #print
