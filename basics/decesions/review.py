0cf8d8c07336dbc2926a7d4bf859d3998ffe65a7
Boldizsar Banfia <boyszy@Boldizsars-MBP.lan>
2021-10-13 11:41:48 +0100

    simple

--- basics/decesions/review.py
+++ basics/decesions/review.py
@@ -7,7 +7,7 @@ if answer2 == ("yes") or ("little"):
     hotel_where = input()
     if hotel_where == ("where?"):
        print("It is just around the corner on your left. Do you need company?")
-        company_answer = input()
+       company_answer = input()
         if company_answer == ("yes") or ("maybe"):
             print("Let us go then, I will show you the way.")
         else:
@@ -15,7 +15,7 @@ if answer2 == ("yes") or ("little"):
     else:
         print('In case you get lost just wistle 2 times and I will find you.')
 
-elif answer2 == ("noo"):
+elif answer2 == ("noo"):#why?? 
     print("Are you in the mood for football?")
     answer3 = input()
     if answer3 == ("yes"):
---
+++ basics/repetitions/while_loop/simple.py
@@ -0,0 +1,21 @@
+#print("How many cable should I remove?")
+#answer = int (input())
+##removed_cables = 0
+#print()
+#while answer < removed_cables:
+    #print("Removed cable")
+    #removed_cables = answer + 1
+
+# Ask user for number of cables
+print("How many cables should I remove?")
+answer1 = int(input())
+
+# Declare a control variable
+cables_removed = 0
+
+# Remove cables
+print()
+
+while cables_removed < answer1:
+    print("Removed cable.")
+    cables_removed = cables_removed + 1 #only the variable not the answer
