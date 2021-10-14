22fd3da45ffa8b9879fe07f44c400aea1047cbd7
Boldizsar Banfia <boyszy@Boldizsars-MBP.lan>
2021-10-14 19:06:06 +0100

    pause

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,8 @@
+def identify():
+    print("What lies before us?")
+    user_response = input()
+    if user_response == "a large boulder":
+        print("It is time to run")
+    else:
+        print("We will be fine")
+identify()
