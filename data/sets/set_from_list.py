1af5a83c5e86b4953434e07c919974c66d626f3d
Banfia Boldizsar <5boldb36@solent.ac.uk>
2021-11-04 12:52:21 +0000

    lab_ends

---
+++ data/sets/set_from_list.py
@@ -0,0 +1,22 @@
+def observed():
+    #  populate the list with 7 values read in from the user.  There should be some duplicate values.
+    observations = []
+    for count in range(7):
+        print("Please enter an observation:")
+        observations.append(input())
+        return observations
+
+def run():
+    print("Counting observations...")
+    local_variable = observed()
+    empty_set = set()
+    # For each value in the list, the function should populate the set as follows:
+    #  create a tuple that contains the value from the list along with a count for how many times that value appears in the list e.g.. ("Skyscraper", 2).
+    for observation in local_variable:
+        data = (observation, local_variable.count(observation))
+        empty_set.add(data)
+
+        for data in local_variable:
+            print(f"{data[0]} observed {data[1]} times")
+
+run()
---
+++ data/sets/simple_set.py
@@ -0,0 +1,7 @@
+def observed():
+    observations = {"Flying Car", "Sky Scraper", "Sky Scraper", "Laser", "Dome", "Dome"}
+    return  observations
+def run():
+    print(observed())
+
+run()
