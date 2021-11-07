08acdd95d8e2ea1a310cd297ad3c9dfa5aa23ebe
boyszy <5boldb36@solent.ac.uk>
2021-11-07 14:33:15 +0000

    end

--- data/olympics/tui.py
+++ data/olympics/tui.py
@@ -21,3 +21,18 @@ def menu():
        """)
     selection = input("Your selection: ")
     return selection.strip().lower()
+
+def display_medal_tally(tally):
+    print(f"| {'Gold':<10} | {tally['Gold']:<10} |")
+    print(f"{'Silver':<10} | {tally['Silver']:<10}|")
+    print(f"| {'Bronze':<10} | {tally['Bronze']:<10} |")
+
+def display_medal_tally(team_tally):
+    for team, tally in team_tally.items():
+        print(team)
+        print(f"\tGold:{tally['Gold']}, Silver:{tally['Silver']}, Bronze:{tally['Bronze']}")
+
+def display_years(years):
+    sorted_years = sorted(years, reverse=True)
+    for year in sorted_years:
+        print(year)
